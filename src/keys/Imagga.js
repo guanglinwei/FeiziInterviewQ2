4b7ef03b68b6c932046d5ef94c1495bbd0d7f697
Guang-Lin Wei <guanglinwei@gmail.com>
2023-03-20 16:47:01 -0400

    added separate file for API keys

--- src/keys/Imagga.js
+++ src/keys/Imagga.js
@@ -1,4 +1,5 @@
 // https://imagga.com/profile/dashboard
 // Image classification is done with Imagga
 // Copy and paste your authorization header here
+// It should look like: Basic XXXXXXXXXXXXXXXX...
 export const AUTH_HEADER = "";
